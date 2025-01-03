a64f16ca78b48514c8c20d545fd0c99ca981456f
unknown <amrebrahem226@gmail.com>
2025-01-03 20:24:47 +0200

    upload image with uploadthing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,8 +18,11 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Navbar />
-        {children}
-        <Footer />
+        <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
+          <div className="flex-1 flex flex-col h-full">{children}</div>
+          <Footer />
+        </main>
+        <Toaster />
       </body>
     </html>
   );
